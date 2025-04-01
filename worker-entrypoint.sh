bd1c4e688c7dbd249971ea642dc69c65754f180d
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-04-01 09:36:45 -0400

    +nodejs

--- worker-entrypoint.sh
+++ worker-entrypoint.sh
@@ -38,13 +38,6 @@ fi
     chown prefect:prefect /home/prefect/.ssh
 }
 
-if [ ! -d "/home/prefect/storage" ]; then
-    mkdir -p "/home/prefect/storage"
-fi
-
-chmod 755 "/home/prefect/storage"
-chown prefect:prefect "/home/prefect/storage"
-
 # Combine CLI-provided WORKER_NAME and INSTANCE for a unique identifier
 UNIQUE_NAME="${WORKER_NAME}-${INSTANCE}"
 echo "Starting Prefect Worker in pool: ${WORK_POOL} with name: ${UNIQUE_NAME}"
