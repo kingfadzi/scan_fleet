d49f6094cc3c46fb20cfe121a3bef9242ae9d36c
kingfadzi <kingfadzi@gmail.com>
2025-04-19 21:38:43 -0400

    pspsutil

--- bin/allinone.sh
+++ bin/allinone.sh
@@ -60,15 +60,8 @@ build_all() {
 --build-arg GRADLE_VERSIONS='${GRADLE_VERSIONS}' \
 --build-arg DEFAULT_GRADLE_VERSION=${DEFAULT_GRADLE_VERSION} \
 --build-arg TOOLS_TARBALL_URL=${TOOLS_TARBALL_URL} \
---build-arg NVM_VERSION=${NVM_VERSION} \
---build-arg NODE_VERSION=${NODE_VERSION} \
---build-arg NVM_NODEJS_ORG_MIRROR=${NVM_NODEJS_ORG_MIRROR} \
---build-arg NVM_PRIVATE_REPO=${NVM_PRIVATE_REPO} \
---build-arg NPM_REGISTRY=${NPM_REGISTRY} \
---build-arg SASS_BINARY=${SASS_BINARY} \
 --build-arg HTTP_PROXY=${HTTP_PROXY} \
 --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
---build-arg NPM_STRICT_SSL=${NPM_STRICT_SSL} \
 --build-arg NO_PROXY=${NO_PROXY}"
 
     docker build --no-cache $BUILD_ARGS -t scanfleet-base -f Dockerfile.base .
